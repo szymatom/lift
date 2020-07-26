b220e6e4dd0ddda6373d2625a30df5e6a0fe2e78
szymatom <szymatom@gmail.com>
2020-07-26 08:01:49 +0200

    initial commit

---
+++ src/main/java/com/example/lift/LiftApplication.java
@@ -0,0 +1,13 @@
+package com.example.lift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiftApplication.class, args);
+	}
+
+}
